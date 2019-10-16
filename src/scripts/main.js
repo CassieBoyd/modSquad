7102b63c324fe7ddf55692b8af8851ede7a5f6b2
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-16 15:59:44 -0500

    Got code working, see comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 {
+    // Added missing comma
     const ModSquad = {
         "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
         "series": {
@@ -7,11 +8,34 @@
         }
     }
 
-    const HTMLRepresentation = `<h1>The Mod Squad</h1>`
+    // Changed const to let for HTMLRepresentation
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
 
+
+    // removed const from HTMLRepresentation as it's already assigned
     ModSquad.members.forEach(member => {
-        const HTMLRepresentation += `<div>${member}</div>`
+        HTMLRepresentation += `<div>${member}</div>`
     })
+    document.querySelector(".show-info").innerHTML = HTMLRepresentation
 }
 
-document.querySelector(".show-info").innerHTML = HTMLRepresentation
+
+
+
+// {
+//     const ModSquad = {
+//         "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"]
+//         "series": {
+//             "start": "1968",
+//             "end": "1973"
+//         }
+//     }
+
+//     const HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+//     ModSquad.members.forEach(member => {
+//         const HTMLRepresentation += `<div>${member}</div>`
+//     })
+// }
+
+// document.querySelector(".show-info").innerHTML = HTMLRepresentation
