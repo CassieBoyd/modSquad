7cc0f34a8ce8630b88cf83259ad989f9501b251a
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-16 18:23:31 -0500

    Added notes describing what the code is doing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,20 +8,27 @@
         }
     }
 
-    // Changed const to let for HTMLRepresentation
+    // Changed const to let for HTMLRepresentation because const can't be reassigned.
     let HTMLRepresentation = `<h1>The Mod Squad</h1>`
 
 
-    // removed const from HTMLRepresentation as it's already assigned
+    // Removed const from HTMLRepresentation in forEach as it's already assigned.
+    // HTMLRepresentation gets updated with each member name as the forEach method
+    // loops through the array.
     ModSquad.members.forEach(member => {
         HTMLRepresentation += `<div>${member}</div>`
     })
+    // Moved querySelector into curly brackets.
+    // querySelector is taking the first class that equals .show-info and putting the
+    // value of HTMLRepresentation into the innerHTML of the tag with that class.
+    // innerHTML refers to the space between the opening and closing tags of the specified
+    // element.
     document.querySelector(".show-info").innerHTML = HTMLRepresentation
 }
 
 
 
-
+// Original Code
 // {
 //     const ModSquad = {
 //         "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"]
